f8f351d161934d9a8972ab5c10ce2d4324482146
Mətləb Əliyev <metleb.aliyev.1997@bk.ru>
2022-12-07 16:47:59 +0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Engin";
+            kurs1.IzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.Egitmen = "Ahmet";
+            kurs2.IzlenmeOrani = 75;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Python";
+            kurs3.Egitmen = "Hasan";
+            kurs3.IzlenmeOrani = 90;
+
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3 };
+            foreach (var kurs in kurslar)
+            {
+                
+                Console.WriteLine(kurs.KursAdi);
+            }
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i].Egitmen);
+            }
+
+            while (kurs1.KursAdi=="C#")
+            {
+                Console.WriteLine("salam");
+                break;
+            }
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int IzlenmeOrani { get; set; }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 5, 8, 9 };
+            int[] b = new int[] { 6, 8, 9 };
+            int[] c = new int[] { 10, 3, 1 };
+
+            a = c;
+            c[1] = 15;
+            Console.WriteLine(a[1]);
+
+        }
+    }
+}
